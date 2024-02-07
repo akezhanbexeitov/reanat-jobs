00bc294ac7e753eb5a9d575707a1eef0ebc5e339
Akezhan <akidulatini@gmail.com>
2024-02-08 01:06:47 +0600

    Finish welcome component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
+import { useFonts } from "expo-font";
+// import { useCallback } from "react";
+// import * as SplashScreen from "expo-splash-screen";
+// import { View } from "react-native";
 import { Stack } from "expo-router";
 
+// SplashScreen.preventAutoHideAsync();
+
 const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  // let onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return <Stack />;
 };
 
